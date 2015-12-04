ad48d3c8ebe7fe444085b1886714e40326a90a0a
correasebastian <correa.m.sebastian@gmail.com>
2015-12-04 23:30:35 +0000

    resolving array of promises

--- models/Job.js
+++ models/Job.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose")
 var Promise = require("bluebird");
-
+mongoose.Promise = require('bluebird');
 var jobSchema = mongoose.Schema({
     title: {
         type: String
@@ -12,25 +12,49 @@ var jobSchema = mongoose.Schema({
 
 var Job = mongoose.model('Job', jobSchema)
 
-exports.seedJobs = function() {
-    return new Promise(function(resolve, reject) {
+var jobs = [{
+    title: 'Cook',
+    description: 'You will be making bagels'
+}, {
+    title: 'Programmer',
+    description: 'You will be mindlessly typing for hours'
+}]
 
-        Job.find({}).exec(function(error, collection) {
+function createP(job) {
+    return new Promise(function(resolve, reject) {
+        Job.create(job,function(err){
+            if (err) 
+            reject()
+            
+            resolve();
+            
+        })
+    })
+}
+exports.seedJobs = function() {
+    //return new Promise(function(resolve, reject) {
+
+   return Job.find({}).exec()
+        .then(function(collection) {
             if (collection.length === 0) {
-                Job.create({
-                    title: 'Cook',
-                    description: 'You will be making bagels'
+                // Job.create({
+                //     title: 'Cook',
+                //     description: 'You will be making bagels'
+                // })
+                // Job.create({
+                //     title: 'Waiter',
+                //     description: 'You will be putting food on peoples tables'
+                // })
+                // Job.create({
+                //     title: 'Programmer',
+                //     description: 'You will be mindlessly typing for hours'
+                // }, resolve)
+
+                return Promise.map(jobs, function(job) {
+                    return createP(job);
                 })
-                Job.create({
-                    title: 'Waiter',
-                    description: 'You will be putting food on peoples tables'
-                })
-                Job.create({
-                    title: 'Programmer',
-                    description: 'You will be mindlessly typing for hours'
-                }, resolve)
             }
         })
 
-    })
+    //})
 }
